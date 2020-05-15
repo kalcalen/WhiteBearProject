4979cecabf8727bcc50f2c9341e55490fc752c6e
kalcalen <kalcalen@gmail.com>
2020-05-15 11:03:48 -0700

    Fixed cards, added toggle feature

--- sript.js
+++ sript.js
@@ -1,3 +1,7 @@
 $("#save-imagery").click(function () {
   $("#overlay-success").toggleClass("d-flex d-none");
 });
+
+$("#sign-up").click(function () {
+  $("#form-toggle").toggleClass("d-none");
+});
