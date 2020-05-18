46b27fc85fc9fa5da427c0c45979212573be7e23
kalcalen <kalcalen@gmail.com>
2020-05-18 11:15:50 -0700

    linked index and create-answer pages

--- sript.js
+++ sript.js
@@ -2,6 +2,10 @@ $("#save-imagery").click(function () {
   $("#overlay-success").toggleClass("d-flex d-none");
 });
 
+$("#back2-answer").click(function () {
+  $("#overlay-error").toggleClass("d-flex d-none");
+});
+
 $("#sign-up").click(function () {
   $("#form-toggle").toggleClass("d-none");
 });
