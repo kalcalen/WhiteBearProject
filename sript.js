9d8694686e8d94e410863967771f7a4bb4072991
kalcalen <kalcalen@gmail.com>
2020-05-20 17:14:20 -0700

    Added button hide to index

--- sript.js
+++ sript.js
@@ -1,3 +1,4 @@
+// Index banners
 $("#save-imagery").click(function () {
   $("#overlay-success").toggleClass("d-flex d-none");
 });
@@ -6,6 +7,17 @@ $("#back2-answer").click(function () {
   $("#overlay-error").toggleClass("d-flex d-none");
 });
 
+// Sign-up button index
 $("#sign-up").click(function () {
   $("#form-toggle").toggleClass("d-none");
+});
+
+$("#sign-up").click(function () {
+  $("#sign-up").hide();
+});
+
+// Error no text
+$("#letsGo").click(function () {
+  var emailInput = $("#emailCreate").val().length;
+  console.log(emailInput);
 });
