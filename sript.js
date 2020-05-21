ca9552545d504abcd1f2540263dea64d0fd8b9a5
kalcalen <kalcalen@gmail.com>
2020-05-21 12:51:40 -0700

    added  input error on index page

--- sript.js
+++ sript.js
@@ -1,23 +1,49 @@
-// Index banners
-$("#save-imagery").click(function () {
-  $("#overlay-success").toggleClass("d-flex d-none");
-});
+$(document).ready(function () {
+  $("#form-toggle").toggle();
+  // Index banners
+  $("#save-imagery").click(function () {
+    $("#overlay-success").toggleClass("d-flex d-none");
+  });
 
-$("#back2-answer").click(function () {
-  $("#overlay-error").toggleClass("d-flex d-none");
-});
+  $("#back2-answer").click(function () {
+    $("#overlay-error").toggleClass("d-flex d-none");
+  });
 
-// Sign-up button index
-$("#sign-up").click(function () {
-  $("#form-toggle").toggleClass("d-none");
-});
+  // Sign-up button index
+  $("#sign-up").click(function () {
+    $("#form-toggle").toggle();
+    $("#sign-up").toggle();
+  });
 
-$("#sign-up").click(function () {
-  $("#sign-up").hide();
-});
+  //$("#sign-up").click(function () {
+  //  $("#sign-up").hide();
+  // });
 
-// Error no text
-$("#letsGo").click(function () {
-  var emailInput = $("#emailCreate").val().length;
-  console.log(emailInput);
+  // Error no text / Success
+  $("#letsGo").click(function () {
+    let emailLength = $("#emailCreate").val().length;
+    let passwordLength = $("#passwordCreate").val().length;
+
+    if (emailLength === 0) {
+      $("#emailCreate").addClass("is-invalid");
+      $("#emailError").html("Please enter a valid email.");
+    } else {
+      $("#emailCreate").removeClass("is-invalid");
+      $("#emailCreate").addClass("is-valid");
+      $("#emailError").html("");
+    }
+
+    if (passwordLength < 9) {
+      $("#passwordCreate").addClass("is-invalid");
+      $("#passwordError").html("Your password must be at least 9 characters");
+      if (passwordLength === 0) {
+        $("#passwordCreate").addClass("is-invalid");
+        $("#passwordError").html("Please enter your password");
+      }
+    } else {
+      $("#passwordCreate").removeClass("is-invalid");
+      $("#passwordCreate").addClass("is-valid");
+      $("#passwordError").html("");
+    }
+  });
 });
